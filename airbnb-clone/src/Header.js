34d82a7bae8737505cf092e3d42088dac822a3c6
samueltiokeng <samueledward40@gmail.com>
2020-09-19 00:46:28 +0200

    img

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className='header'>
-            <img
-            
-            />
-        </div>
-    )
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://i.pinimg.com/originals/3c/bf/be
+              3cbfbe148597341fa56f2f87ade90956.png"
+        alt="logo"
+      />
+    </div>
+  );
 }
 
-export default r
+export default ;
