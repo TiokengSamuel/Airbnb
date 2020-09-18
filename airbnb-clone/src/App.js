4459c05de08d19522bda1d6b2a0b72395b543d49
samueltiokeng <samueledward40@gmail.com>
2020-09-19 00:51:55 +0200

    lo

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home"
+import Header from "./Header"
 
 function App() {
   return (
--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -6,8 +6,7 @@ function Header() {
     <div className="header">
       <img
         className="header__icon"
-        src="https://i.pinimg.com/originals/3c/bf/be
-              3cbfbe148597341fa56f2f87ade90956.png"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt="logo"
       />
     </div>
