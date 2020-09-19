f7ea19ab4f5095ae14eade0917d3200efeff0238
samueltiokeng <samueledward40@gmail.com>
2020-09-19 23:11:16 +0200

    c

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -28,22 +28,22 @@ function Home() {
             </div>
             <div className="home__section">
                 <Card
-                src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
-                title = "3 Bedroom Flat in Bournemouth"
-                description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                price="$130/night"
+                    src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+                    title = "3 Bedroom Flat in Bournemouth"
+                    description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+                    price="$130/night"
                 />
                 <Card
-                src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-                title = "Penthouse in London"
-                description="Enjoy the amazing sight of London with this stunning penthouse"
-                price="$350/night"
+                    src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+                    title = "Penthouse in London"
+                    description="Enjoy the amazing sight of London with this stunning penthouse"
+                    price="$350/night"
                 />
                 <Card
-                src="https://media.nomadicmatt.com/2018/apartment.jpg"
-                title = "1 Bedroom apartment"
-                description="Superhost with great amnities and a fabolous shopping complex nearby"
-                price="$70/night"
+                    src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                    title = "1 Bedroom apartment"
+                    description="Superhost with great amnities and a fabolous shopping complex nearby"
+                    price="$70/night"
                 />
             </div>
         </div>
