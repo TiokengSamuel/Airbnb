e219563f8dc7d953c8beee5894ff4bfcfa1e0629
samueltiokeng <samueledward40@gmail.com>
2020-09-19 11:02:37 +0200

    b

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -5,7 +5,14 @@ import {Button} from "@material-ui/core"
 function Banner() {
     return (
         <div className="banner">
-            
+            <div className='banner__info'>
+                <h1>Get out and strech your imagination</h1>
+                <h5>Plan a different kind of gateway
+                    to uncover the hidden gems near you.
+                </h5>
+                <Button variant='outlined'>
+                    Explore Nearby</Button>
+            </div>
         </div>
     )
 }
