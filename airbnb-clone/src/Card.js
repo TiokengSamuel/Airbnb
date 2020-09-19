fec9056d319322ad98a636197afb453d028f89f6
samueltiokeng <samueledward40@gmail.com>
2020-09-19 19:45:13 +0200

    c

--- airbnb-clone/src/Card.js
+++ airbnb-clone/src/Card.js
@@ -7,7 +7,8 @@ function Card({src, title, description, price}) {
             <img src = {src} alt=""/>
             <div className="card__info">
                 <h2>{title}</h2>
-                <h4>{description</h4>
+                <h4>{description}</h4>
+                <h3>{price}</h3>
             </div>
         </div>
     )
