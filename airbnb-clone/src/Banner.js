654c1e329e8fda333031b3635f74b67ddc7631d3
samueltiokeng <samueledward40@gmail.com>
2020-09-19 08:18:39 +0200

    r

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -4,7 +4,12 @@ import './Banner.css'
 function Banner() {
     return (
         <div className="banner">
-            <div className="banner__info"></div>
+            <div className="banner__info">
+                <h1>Get out and strech your imagination</h1>
+                <h5>Plan a different kind of gateway
+                    to uncover the hidden gems near you.
+                </h5>
+            </div>
         </div>
     )
 }
