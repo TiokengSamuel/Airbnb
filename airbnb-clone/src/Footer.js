b78dd6ee941d68319031ccb013df734012789115
samueltiokeng <samueledward40@gmail.com>
2020-09-19 23:29:10 +0200

    footer

--- airbnb-clone/src/Footer.js
+++ airbnb-clone/src/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <p>2020 Airbnb clone! No rights reserved</p>
+            <p>By xaster_Kies @ Lamp</p>
         </div>
     )
 }
