857374672e983ef3e519b6414751b25195a2297c
samueltiokeng <samueledward40@gmail.com>
2020-09-19 21:07:40 +0200

    c

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -18,7 +18,7 @@ function Home() {
                 <Card
                 src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
                 title="Unique Stays"
-                description="Spaces that are mor"
+                description="Spaces that are more than just a place to sleep"
                 />
                 <Card
                 src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
