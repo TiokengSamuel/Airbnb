fb356dfa337f99231c1d36d218e1d34edceba513
samueltiokeng <samueledward40@gmail.com>
2020-09-20 11:03:32 +0200

    s

---
+++ airbnb-clone/src/SearchResult.js
@@ -0,0 +1,12 @@
+import React from 'react'
+im
+
+function SearchResult() {
+    return (
+        <div className="searchResult">
+            
+        </div>
+    )
+}
+
+export default SearchResult
